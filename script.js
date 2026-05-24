8ac67f42e689429a2d04d6f1074454d80ea4f71e
himangshu3226 <himangshuk827@gmail.com>
2026-05-24 21:24:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+body{
+    background-color: black;
+    color: white;
+    text-align: center;
+    margin-top: 100px;
+    font-family: Arial;
+}
+
+button{
+    padding: 10px 20px;
+    font-size: 18px;
+    background-color: red;
+    color: white;
+    border: none;
+    border-radius: 10px;
+}
